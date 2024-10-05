454068502543e60c7da73bf697e014afb11be055
febits <febits@proton.me>
2024-10-05 16:40:50 -0300

    basic main task: exec loop showing subathon timer on serial monitor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use anyhow::Result;
 
-    // Bind the log crate to the ESP Logging facilities
+use meiaclock_esp32::api::SubathonAPI;
+use meiaclock_esp32::wifi::start_wifi;
+
+use std::thread;
+use std::time;
+
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let wifi = start_wifi(env!("SSID"), env!("PASSWORD"))?;
+    let wifi = wifi.wifi();
+
+    log::info!("{:?}", wifi.sta_netif().get_ip_info()?);
+
+    let mut api = SubathonAPI::new(env!("API_URL"))?;
+
+    loop {
+        let timer = api.get_time_left()?;
+        log::info!(
+            "{:02}:{:02}:{:02}",
+            timer.hours,
+            timer.minutes,
+            timer.seconds
+        );
+        thread::sleep(time::Duration::from_millis(500));
+    }
 }
