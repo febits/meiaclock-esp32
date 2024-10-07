7c5b0d3054a622b44d1c3936706b794d8a6c3565
febits <febits@proton.me>
2024-10-07 00:07:53 -0300

    improved the exec loop drawing the timer to the display

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,62 @@
 use anyhow::Result;
 
 use meiaclock_esp32::api::SubathonAPI;
+use meiaclock_esp32::display::Display;
 use meiaclock_esp32::wifi::start_wifi;
 
 use std::thread;
-use std::time;
+use std::time::{Duration, Instant};
+
+use esp_idf_svc::hal::gpio::AnyIOPin;
+use esp_idf_svc::hal::peripherals::Peripherals;
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let wifi = start_wifi(env!("SSID"), env!("PASSWORD"))?;
-    let wifi = wifi.wifi();
+    let peripherals = Peripherals::take()?;
 
-    log::info!("{:?}", wifi.sta_netif().get_ip_info()?);
+    let wifi = start_wifi(env!("SSID"), env!("PASSWORD"), peripherals.modem)?;
+    log::debug!("{:?}", wifi.wifi().sta_netif().get_ip_info()?);
 
     let mut api = SubathonAPI::new(env!("API_URL"))?;
+    let mut display = Display::new(
+        peripherals.i2c0,
+        unsafe { AnyIOPin::new(env!("SDA_PIN").parse::<i32>()?) },
+        unsafe { AnyIOPin::new(env!("SCL_PIN").parse::<i32>()?) },
+    )?;
+
+    display.ddriver.init();
+    display.draw_meianatal();
+
+    thread::sleep(Duration::from_secs(2));
 
     loop {
-        let timer = api.get_time_left()?;
-        log::info!(
-            "{:02}:{:02}:{:02}",
-            timer.hours,
-            timer.minutes,
-            timer.seconds
+        let start = Instant::now();
+        let timer = match api.get_time_left() {
+            Ok(t) => t,
+            Err(_) => {
+                log::error!("Failed to get timeLeft from API. Restarting the system...");
+                unsafe {
+                    esp_idf_svc::sys::esp_restart();
+                }
+            }
+        };
+
+        display.draw_timer(
+            format!(
+                "{:02}:{:02}:{:02}",
+                timer.hours, timer.minutes, timer.seconds
+            )
+            .as_str(),
         );
-        thread::sleep(time::Duration::from_millis(500));
+
+        let elapsed = start.elapsed();
+
+        if elapsed < Duration::from_millis(1000) {
+            thread::sleep(Duration::from_millis(1000) - elapsed);
+        }
     }
+
+    Ok(())
 }
