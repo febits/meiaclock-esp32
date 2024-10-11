8d30f269e376ec3df7bcb048dffd14cf1b5afd77
febits <febits@proton.me>
2024-10-11 18:31:58 -0300

    adding a time window which helps the API server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use meiaclock_esp32::api::SubathonAPI;
+use meiaclock_esp32::api::{SubathonAPI, SubathonTimer};
 use meiaclock_esp32::display::Display;
 use meiaclock_esp32::wifi::start_wifi;
 
@@ -10,8 +10,10 @@ use std::time::{Duration, Instant};
 use esp_idf_svc::hal::gpio::AnyIOPin;
 use esp_idf_svc::hal::peripherals::Peripherals;
 
+const WINDOW_MAX: Duration = Duration::from_millis(20000);
+
 fn restart_system(msg: String) -> ! {
-    log::error!("{}", format!("{msg}. Restarting the system..."));
+    log::error!("{msg}. Restarting the system...");
     unsafe {
         esp_idf_svc::sys::esp_restart();
     }
@@ -46,24 +48,41 @@ fn main() -> Result<()> {
 
     thread::sleep(Duration::from_secs(2));
 
+    let mut window = Duration::from_millis(0);
+    let mut elapsed = Duration::from_millis(0);
+    let mut timer = SubathonTimer::from(0);
+
+    let mut starting_now = true;
+
     loop {
+        if window >= WINDOW_MAX || starting_now {
+            let start = Instant::now();
+            timer = api.get_time_left().unwrap_or_else(|e| {
+                restart_system(format!("Failed to get timeLeft from API: {e}"));
+            });
+            window = Duration::from_millis(0);
+
+            log::info!("Timer updated: {:?}", start.elapsed());
+
+            if starting_now {
+                starting_now = false;
+            }
+
+            elapsed += start.elapsed();
+        } else {
+            let start = Instant::now();
+            timer = SubathonTimer::from(Into::<u64>::into(timer) - 1000);
+            elapsed += start.elapsed();
+        }
         let start = Instant::now();
+        display.draw_timer(&timer);
+        elapsed += start.elapsed();
 
-        let timer = api.get_time_left().unwrap_or_else(|e| {
-            restart_system(format!("Failed to get timeLeft from API: {e}"));
-        });
-        display.draw_timer(
-            format!(
-                "{:02}:{:02}:{:02}",
-                timer.hours, timer.minutes, timer.seconds
-            )
-            .as_str(),
-        );
-
-        let elapsed = start.elapsed();
-
-        if elapsed < Duration::from_millis(1000) {
+        if elapsed <= Duration::from_millis(1000) {
             thread::sleep(Duration::from_millis(1000) - elapsed);
         }
+
+        window += Duration::from_millis(1000);
+        elapsed = Duration::from_millis(0);
     }
 }
